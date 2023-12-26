9664453928a680f730f1c74acb8074f428c621f6
Iulian Constantin Constantinescu <iulian@iconmedia.ro>
2023-12-26 11:47:06 +0200

    Update ConnectingModal.svelte to make the modal not dismissable

--- src/routes/modals/ConnectingModal.svelte
+++ src/routes/modals/ConnectingModal.svelte
@@ -3,7 +3,7 @@
 	import { tastytradeSession } from '$lib/tastytrade'
 </script>
 
-<Modal open={$tastytradeSession == 'checking'} size="xs" permanent>
+<Modal open={$tastytradeSession == 'checking'} size="xs" dismissable={false}>
 	<div class="text-center">
 		<Spinner color="blue" size="12" />
 		<p class="text-center mt-4 text-black font-semibold text-xl">Connecting...</p>
