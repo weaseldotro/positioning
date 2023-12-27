d4a048fef17d0f0156bad82935dd50ebb960000d
Iulian Constantin Constantinescu <iulian@iconmedia.ro>
2023-12-27 12:03:09 +0200

    Update AuthorizingModal.svelte to disable dismissable option in Modal component

--- src/routes/modals/AuthorizingModal.svelte
+++ src/routes/modals/AuthorizingModal.svelte
@@ -22,7 +22,7 @@
 	}
 </script>
 
-<Modal open={$tastytradeSession == 'invalid'} size="xs" autoclose={false} permanent class="w-full">
+<Modal open={$tastytradeSession == 'invalid'} size="xs" autoclose={false} dismissable={false} class="w-full">
 	<form class="flex flex-col space-y-6" on:submit={authorize}>
 		<h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">Please login with your tastytrade account</h3>
 		<Label class="space-y-2">
@@ -35,7 +35,7 @@
 		</Label>
 		<Button color="blue" type="submit" class="w-full1" disabled={working}>Login</Button>
 		{#if showError}
-			<Toast simple color="red" class="mb-2">
+			<Toast color="red" class="mb-2">
 				<svelte:fragment slot="icon">
 					<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
 					<span class="sr-only">Error</span>
